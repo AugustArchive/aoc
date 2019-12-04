b618ead557233e87bfb1e7b955b5169dc9f14467
August (Chris) <augu@voided.pw>
2019-12-04 07:04:21 -0700

    Work on Day 2, added JSDoc for utility functions

--- 2019/Day1/part2.js
+++ 2019/Day1/part2.js
@@ -3,6 +3,11 @@ const { readFileSync } = require('fs');
 const input = readFileSync('./input.txt', 'utf8');
 const inputs = input.split('\n');
 
+/**
+ * Calculates the fuel and recursives if `fuel` is over 0
+ * @param {number} value The value to formulate the check from
+ * @returns {number} It returns `0` if the fuel went below 0 or returns a value if it went over `0`
+ */
 function calculateFuel(value) {
     const fuel = (Math.floor(value / 3) - 2);
     if (fuel > 0) {
--- 2019/Day2/part1.js
+++ 2019/Day2/part1.js
@@ -3,3 +3,24 @@ const { readFileSync } = require('fs');
 const file   = readFileSync('./input.txt', 'utf8');
 const inputs = file.split(',');
 
+/**
+ * Gets the OPCode, splices from the `inputs` array and returns the opcode and the 3 arguments it requires
+ * 
+ * **NOTE**: OPCode 99 doesn't require any arguments, so it just returns an empty array
+ * 
+ * @returns {{ opcode: string; arguments: [string, string, string]; }} The OPCode (`1`) and the 3 arguments it requires
+ */
+function getOpcode() {
+    const [opcode, arg1, arg2, arg3] = inputs;
+
+    if (opcode === '99') return { opcode: '99', arguments: [] };
+    inputs.splice(0, 4);
+    return {
+        opcode,
+        arguments: [arg1, arg2, arg3]
+    };
+}
+
+// debugging this for now
+const { opcode, arguments: args } = getOpcode();
+console.log(`OPCode: ${opcode}\nArguments: ${args.join(' | ')}`)
